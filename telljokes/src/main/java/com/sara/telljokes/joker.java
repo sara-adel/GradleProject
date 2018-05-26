e2bb12ad95b55be6a8bfb0c806ef1886599b2e59
sara adel <saraadel895@gmail.com>
2018-05-26 15:36:54 +0200

    Initial commit

---
+++ telljokes/src/main/java/com/sara/telljokes/joker.java
@@ -0,0 +1,8 @@
+package com.sara.telljokes;
+
+public class joker {
+
+    public String getJoke() {
+        return "*During an interview*\nInterviewer: 'So how long were you employed in your last job?'\nCandidate: 'I'd say my biggest weakness is my listening skills.'";
+    }
+}
